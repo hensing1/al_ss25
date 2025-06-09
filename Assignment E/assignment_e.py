422e6f61f347fde3072f33fd07ed9a71e1717d89
hensing1 <web@jhlehmann.de>
2025-06-10 00:18:17 +0200

    stub assignment e

---
+++ Assignment E/assignment_e.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from sys import argv
+from typing import NamedTuple
+
+class City(NamedTuple):
+    id: int  # not really needed, but it's there and it doesn't hurt
+    name: str
+    pos_x: int
+    pos_y: int
+
+
+class Individuum(NamedTuple):
+    genome: list[int]  # list of indices of cities
+
+
+def parse(file: Path) -> list[City]:
+    if not file.exists() or not file.is_file():
+        raise ValueError(f"File {file.absolute()} not found.")
+
+    cities = []
+    with open(file) as f:
+        for line in f.readlines():
+            if line.startswith('#'):
+                continue
+            id, pos_x, pos_y, name = map(str.strip, line.split('\t'))
+            cities.append(City(int(id), name[1:], int(pos_x), int(pos_y)))
+    return cities
+
+
+def distance(a: City, b: City) -> float:
+    dx = b.pos_x - a.pos_x
+    dy = b.pos_y - a.pos_y
+    return (dx * dx + dy * dy) ** 0.5
+
+
+def fitness(genome: list[int], cities: list[City]) -> float:
+    score = 0
+    for i in range(len(genome) - 1):
+        score += distance(cities[genome[i]], cities[genome[i+1]])
+    return score
+
+
+def is_valid(genome: list[int], cities: list[City]) -> bool:
+    if len(genome) != 2 * len(cities):
+        return False
+    
+    for i in range(len(cities)):
+        if genome.count(i) != 2:
+            return False
+        
+    return True
+
+
+
+def main():
+    if len(argv) != 2:
+        raise ValueError(f"Usage: {argv[0]} <path_to_cites_file>")
+    cities = parse(Path(argv[1]))
+    for city in cities:
+        print(city)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment E/ideen.md
@@ -0,0 +1,20 @@
+## Genom
+
+- Jede Stadt zwei mal
+- Reihenfolge relevant
+
+## Vererbung
+
+### 1
+1 crossover: Alle Städte von Parent 1 bis cross-over, dann übrige Reihenfolge von parent 2
+
+### 2
+- 1 crossover
+- eins von parent 1, das andere von parent 2
+- im resultierenden Genom:
+    - falls Stadt mehr als 2x, behalte die mit größter Distanz zu Nachbarn
+    - falls Stadt fehlt: einsortieren in Stelle von größter Distanz?
+
+
+## Mutation
+
